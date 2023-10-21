a1b81626a9df5cba4e2661c8bf3ee8881bb9b443
You-Jia Huang <yojahuang30613@gmail.com>
2023-10-21 15:06:13 +0800

    feat: rearrange file structure and add db uri

---
+++ DBModel/User.py
@@ -0,0 +1,9 @@
+from app import db
+
+
+# Define the model with columns id (user id or group id), session_id, grammar_on, caption_on
+class User(db.Model):
+    id = db.Column(db.String, primary_key=True)
+    session_id = db.Column(db.String)
+    grammar_on = db.Column(db.Boolean, default=False)
+    caption_on = db.Column(db.Boolean, default=False)
